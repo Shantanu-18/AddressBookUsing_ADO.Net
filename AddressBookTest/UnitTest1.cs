43269b7340ab9793af2112b18ad2e86c9f9e2fdc
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-08-15 00:51:59 +0530

    [Shantanu] Added : Test method to check edit and delete method are giving true output.

--- AddressBookTest/UnitTest1.cs
+++ AddressBookTest/UnitTest1.cs
@@ -6,24 +6,55 @@ namespace AddressBookTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            AddressBook addressBook = new AddressBook();
-            AddressBookModel model = new AddressBookModel();
+        AddressBook addressBook = new AddressBook();
+        AddressBookModel model = new AddressBookModel();
 
-            model.FirstName = "Shantanu";
-            model.LastName = "Borkar";
-            model.Address = "007 - Walls street";
-            model.City = "Austin";
+        [TestMethod]
+        public void AddingContactShouldReturnTrue()
+        {
+            model.FirstName = "Tom";
+            model.LastName = "Cruse";
+            model.Address = "007 - Breakers street";
+            model.City = "Vegas";
             model.State = "Texas";
             model.ZipCode = "73301";
             model.PhoneNumber = "1122334455";
-            model.EmailId = "Shantanu@xyz.com";
+            model.EmailId = "007@xyz.com";
 
             var result = addressBook.AddContacts(model);
             Assert.IsTrue(result);
+        }
 
+        [TestMethod]
+        public void UpdatingContactShouldReturnTrue()
+        {
+            model.FirstName = "Tom";
+            model.LastName = "Holland";
+            model.Address = "Cental park";
+            model.City = "New york";
+            model.State = "New york";
+            model.ZipCode = "668899";
+            model.PhoneNumber = "8888888888";
+            model.EmailId = "spider@xyz.com";
+
+            var result = addressBook.EditContact(model);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DelectingContactShouldReturnTrue()
+        {
+            model.FirstName = "Tony";
+            model.LastName = "Cruse";
+            model.Address = "007 - Breakers street";
+            model.City = "Vegas";
+            model.State = "Mexico";
+            model.ZipCode = "771122";
+            model.PhoneNumber = "6699332211";
+            model.EmailId = "007@xyz.com";
+
+            var result = addressBook.DeleteContact(model);
+            Assert.IsTrue(result);
         }
     }
 }
